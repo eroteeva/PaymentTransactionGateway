494301d2818418b21b3ff80fa40ac29731588342
Teodora <tlyubomirova@gmail.com>
2020-04-20 15:20:22 -0400

    CHANGE: Test config implementation

---
+++ src/main/java/testApp/APIResponseTests.java
@@ -0,0 +1,11 @@
+package testApp;
+
+public class APIResponseTests {
+
+    public static void main(String args[]) throws Exception {
+
+        TestsConfig testsConfig = new TestsConfig(args);
+
+    }
+
+}
---
+++ src/main/java/testApp/TestsConfig.java
@@ -0,0 +1,35 @@
+package testApp;
+
+import org.apache.commons.validator.routines.UrlValidator;
+
+public class TestsConfig {
+
+    private String endpoint;
+    private String transactionType;
+
+    public TestsConfig(String args[]) throws Exception {
+        this.endpoint = args[0];
+        this.transactionType = args[1];
+        this.setEndpoint(args[0]);
+    }
+
+    public void setEndpoint(String endpoint) throws Exception {
+
+        String[] schemes = {"http","https"};
+
+        UrlValidator urlValidator = new UrlValidator(schemes);
+        if (!urlValidator.isValid(endpoint)){
+            throw new Exception(" ");
+        }
+
+        this.endpoint = endpoint;
+    }
+
+    public String getEndpoint(){
+        return endpoint;
+    }
+
+    public String getTransactionType(){
+        return transactionType;
+    }
+}
